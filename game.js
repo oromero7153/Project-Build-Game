c69cba2c522f3fa582dc7fa220f57496bd672f0c
oromero7153 <131738715+oromero7153@users.noreply.github.com>
2023-06-23 10:23:19 -0700

    added pseudocode to JS

---
+++ game.js
@@ -0,0 +1,13 @@
+//need code to generate code that will populate a character when the player clicks on the buttons. eventlistener with click event. 
+
+
+//need code that will finalize the players choice when they click the submit button and trigger the mathrandom function to generate the computer's choice.
+
+
+//need a function that will generate the "rock, paper, scissors"
+
+
+//after mathrandom functions triggers, the  'winning' character will animate 
+
+
+// function that triggers winning player with phrase that says "... wins!"
