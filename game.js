b86ebb8a1ea23597b6202357f59f3bce64fd72b1
oromero7153 <131738715+oromero7153@users.noreply.github.com>
2023-06-24 13:26:31 -0700

    figuring getting the characters by click

--- game.js
+++ game.js
@@ -1,6 +1,14 @@
 //need code to generate code that will populate a character when the player clicks on the buttons. eventlistener with click event. 
 
 
+let knightCharacter = document.createElement('img')
+knightCharacter.src = 'Assets/flyingDragonRight.gif'
+knightCharacter.style.position = 'fixed'
+knightCharacter.style.left = '25%'
+knightCharacter.style.bottom = '25%'
+// knightCharacter.style.display = 'none'
+document.body.append(knightCharacter)
+
 //need code that will finalize the players choice when they click the submit button and trigger the mathrandom function to generate the computer's choice.
 
 
