b155a27c8ac92c0e5a7d51cb418d87d9899e115e
oromero7153 <131738715+oromero7153@users.noreply.github.com>
2023-06-24 14:23:29 -0700

    managed to add characters on button click

--- game.js
+++ game.js
@@ -1,13 +1,30 @@
 //need code to generate code that will populate a character when the player clicks on the buttons. eventlistener with click event. 
 
+const dragonButton = document.getElementById("dragon");
+const dragonRight = document.getElementById("dragonFacingRight");
+dragonButton.addEventListener("click", () => {
+    dragonRight.style.display = "block";
+});
 
-let knightCharacter = document.createElement('img')
-knightCharacter.src = 'Assets/flyingDragonRight.gif'
-knightCharacter.style.position = 'fixed'
-knightCharacter.style.left = '25%'
-knightCharacter.style.bottom = '25%'
-// knightCharacter.style.display = 'none'
-document.body.append(knightCharacter)
+const knightButton = document.getElementById("knight");
+const knightRight = document.getElementById("knightRight");
+knightButton.addEventListener("click", () => {
+    knightRight.style.display = "block";
+});
+
+const princessButton = document.getElementById("princess");
+const princessRight = document.getElementById("princessRight");
+princessButton.addEventListener("click", () => {
+    princessRight.style.display = "block";
+});
+
+/*let dragonCharacter = document.createElement('img')
+dragonCharacter.src = 'Assets/flyingDragonRight.gif'
+dragonCharacter.style.position = 'fixed'
+dragonCharacter.style.left = '25%'
+dragonCharacter.style.bottom = '25%'
+// dragonCharacter.style.display = 'none'
+document.body.append(dragonCharacter)*/
 
 //need code that will finalize the players choice when they click the submit button and trigger the mathrandom function to generate the computer's choice.
 
