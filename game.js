3e822235b09535ce7277ee226e0013e342ed01e7
oromero7153 <131738715+oromero7153@users.noreply.github.com>
2023-06-24 14:38:59 -0700

    added read me

--- game.js
+++ game.js
@@ -6,17 +6,19 @@ dragonButton.addEventListener("click", () => {
     dragonRight.style.display = "block";
 });
 
-const knightButton = document.getElementById("knight");
-const knightRight = document.getElementById("knightRight");
-knightButton.addEventListener("click", () => {
-    knightRight.style.display = "block";
-});
+// const knightButton = document.getElementById("knight");
+// const knightRight = document.getElementById("knightRight");
+// knightButton.addEventListener("click", () => {
+//     knightRight.style.display = "block";
+// });
 
-const princessButton = document.getElementById("princess");
-const princessRight = document.getElementById("princessRight");
-princessButton.addEventListener("click", () => {
-    princessRight.style.display = "block";
-});
+// const princessButton = document.getElementById("princess");
+// const princessRight = document.getElementById("princessRight");
+// princessButton.addEventListener("click", () => {
+//     princessRight.style.display = "block";
+// });
+
+
 
 /*let dragonCharacter = document.createElement('img')
 dragonCharacter.src = 'Assets/flyingDragonRight.gif'
---
+++ readMe.md
@@ -0,0 +1,9 @@
+                Princess, Knight, Dragon
+This game is a variation of Rock, Paper, Scissors but more dynamic with characters populating the browser. 
+
+The rules are simple:
+    The Knight defeats the Dragon with swordsmanship.
+    The Princess defeats the Knight with skill and intelligence.
+    The Dragon defeats the Princess by kidnapping her. 
+
+The game is player vs the computer. The player will select their character and submit their selection by hitting the "fight" button. This will trigger the computer to select a random character. A function will determine the winner and trigger a statement declaring the winner. 
