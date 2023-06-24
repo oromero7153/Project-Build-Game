78f4ff36994752ab0cd13bf6f038021a1b803775
oromero7153 <131738715+oromero7153@users.noreply.github.com>
2023-06-24 15:36:51 -0700

    added characters to appear and disappear

--- game.js
+++ game.js
@@ -4,29 +4,26 @@ const dragonButton = document.getElementById("dragon");
 const dragonRight = document.getElementById("dragonFacingRight");
 dragonButton.addEventListener("click", () => {
     dragonRight.style.display = "block";
+    knightRight.style.display = "none";
+    princessRight.style.display = "none";
 });
 
-// const knightButton = document.getElementById("knight");
-// const knightRight = document.getElementById("knightRight");
-// knightButton.addEventListener("click", () => {
-//     knightRight.style.display = "block";
-// });
+const knightButton = document.getElementById("knight");
+const knightRight = document.getElementById("knightRight");
+knightButton.addEventListener("click", () => {
+    knightRight.style.display = "block";
+    dragonRight.style.display = "none";
+    princessRight.style.display = "none";
+});
 
-// const princessButton = document.getElementById("princess");
-// const princessRight = document.getElementById("princessRight");
-// princessButton.addEventListener("click", () => {
-//     princessRight.style.display = "block";
-// });
+const princessButton = document.getElementById("princess");
+const princessRight = document.getElementById("princessRight");
+princessButton.addEventListener("click", () => {
+    princessRight.style.display = "block";
+    knightRight.style.display = "none";
+    dragonRight.style.display = "none";
+});
 
-
-
-/*let dragonCharacter = document.createElement('img')
-dragonCharacter.src = 'Assets/flyingDragonRight.gif'
-dragonCharacter.style.position = 'fixed'
-dragonCharacter.style.left = '25%'
-dragonCharacter.style.bottom = '25%'
-// dragonCharacter.style.display = 'none'
-document.body.append(dragonCharacter)*/
 
 //need code that will finalize the players choice when they click the submit button and trigger the mathrandom function to generate the computer's choice.
 
