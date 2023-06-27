da48aa6ffec475f50ca043daa2e8bd8037823ebb
oromero7153 <131738715+oromero7153@users.noreply.github.com>
2023-06-26 21:28:36 -0700

    trying to create login in JS

--- game.js
+++ game.js
@@ -1,4 +1,4 @@
-//need code to generate code that will populate a character when the player clicks on the buttons. eventlistener with click event. 
+// code that will populate a character when the player clicks on the buttons. 
 
 const dragonButton = document.getElementById("dragon");
 const dragonRight = document.getElementById("dragonFacingRight");
@@ -25,23 +25,62 @@ princessButton.addEventListener("click", () => {
 });
 
 
-//need code that will finalize the players choice when they click the submit button and trigger the mathrandom function to generate the computer's choice.
-// let playerSelection = 
-// let computerSelection = Math.random();
-if (computerSelection < 0.34) {
-    computerSelection = "dragon";
-} else if (computerSelection <= 0.67) {
-    computerSelection = "knight";
-} else {
-    computerSelection = "princess";
-}
-// const gamePlay = () => {
-//     const dragonButton = document.getElementById("dragon");
-//     const knightButton = document.getElementById("knight");
-//     const princessButton = document.getElementById("princess");
-//     const playerOptions = [dragonButton, knightButton, princessButton];
+//adding click event to buttons in order to play the game
 
-//after mathrandom functions triggers, the  'winning' character will animate 
+function test(){
+    const buttons = document.querySelectorAll(".buttons")
+        let player1;
+        let computer;
+
+    buttons.forEach(button => button.addEventListener("click", () => {
+        player1 = button.textContent;
+        computerPick();
+        result();
+    } ))
+} console.log(test())
+//the function that runs the math random for the computer pick
+    function computerPick(){
+    let computer = Math.random();
+      if (computer < 0.34) {
+    computer = "dragon";
+         } else if (computer <= 0.67) {
+    computer = "knight";
+         } else {
+    computer = "princess";
+    }
+};
+//the function that compares the choices
+function result(){
+    if(player1===computer){
+        return "The result is a tie!";
+    }
+    else if(player1==="dragon"){
+        if(computer==="princess"){
+            return "Player 1 Wins!"
+        }
+        else if(player1==="knight"){
+            return "The Computer Wins";
+        }
+    }
+    else if(player1==="princess"){
+        if(computer==="knight"){
+            return "Player 1 Wins"
+        }
+        else if(player1==="dragon"){
+            return "The Computer Wins";
+        }
+    }
+    else if(player1==="knight"){
+        if(computer==="dragon"){
+            return "Player 1 Wins"
+        }
+        else if(computer==="princess"){
+            return "The Computer Wins";
+        }
+    }    
+}
+//console.log(test())
+ 
 
 
 // function that triggers winning player with phrase that says "... wins!"
